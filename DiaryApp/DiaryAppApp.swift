8f6df619b63e1069f2f523114ae9228d129cb545
mnakata <massan199627@gmail.com>
2025-04-28 12:21:14 +0900

    Initial Commit

---
+++ DiaryApp/DiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryAppApp.swift
+//  DiaryApp
+//
+//  Created by Masahiko Nakata on 2025/04/28.
+//
+
+import SwiftUI
+
+@main
+struct DiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiaryAppTests/DiaryAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryAppTests.swift
+//  DiaryAppTests
+//
+//  Created by Masahiko Nakata on 2025/04/28.
+//
+
+import Testing
+
+struct DiaryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
